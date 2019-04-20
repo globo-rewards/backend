7754b940738c90e9c6b5d54cdf93a07c0747cf13
Matheus Catossi <mcatossi@br.ibm.com>
2019-04-20 20:57:48 -0300

    added first endpoint and add connection with database

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log("hey");
+const AssistantV1 = require('watson-developer-cloud/assistant/v1');
+var express = require('express');
+var app = express();
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var connFactory = require("./connection/connFactory.js");
+
+var cors = require('cors');
+app.use(cors())
+
+let Promise = require('bluebird');
+
+let paramsCloudant = {
+    "username": "96ba32ad-e17d-494f-a93e-72240b1e0b16-bluemix",
+    "host": "96ba32ad-e17d-494f-a93e-72240b1e0b16-bluemix.cloudant.com",
+    "dbname": "globo-rewards",
+    "password": "e373c010bcb53c3ea89a59f7fa2642789e7bfe3128bab1c3e2762b713ab04641"
+};
+
+app.get('/', function (req, res) {
+    res.send("response");
+});
+
+
+var port = process.env.PORT || 3001
+app.listen(port, function () {
+    console.log("To view your app, open this link in your browser: http://localhost:" + port);
+});
